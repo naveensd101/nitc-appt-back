5c9bd0b22690979567fd579de0f5e9a118df8685
Naveen <naveensd101@gmail.com>
2021-12-01 21:26:18 +0530

    List all fac function added
    Other UPDs:
     - Call `/list_all_fac` to get the list of fac names.
     - Had to change all the tabs of the files with 4 spaces.
        - Apparently python was designed like that and its better to use
          that.
        - Real reason is that I couldnt figre out how to auto indent with 2
          tabs in vim.

--- __init__.py
+++ __init__.py
@@ -21,159 +21,177 @@ dotenv_path = join(os.path.dirname(os.path.realpath(__file__)), '.env')
 load_dotenv(dotenv_path)
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
-
 up.uses_netloc.append("postgres")
 url = up.urlparse(DATABASE_URL)
-dbconn = psycopg2.connect(database=url.path[1:],
-user=url.username,
-password=url.password,
-host=url.hostname,
-port=url.port
+dbconn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
 )
 
 
 
 def create_app():
 #create_app is a keyword.
-	app= Flask(__name__)
-	#CORS(app)
-	app.config.from_mapping(
-		DATABASE= "NITC-ApptMgmt"
-	)
+    app= Flask(__name__)
+    #CORS(app)
+    app.config.from_mapping(
+        DATABASE= "NITC-ApptMgmt"
+    )
 
 #########################################################################################################################
 
-	@app.route("/")
-	def index():
-		response = jsonify(message="The server is running")
-		return response
+    @app.route("/")
+    def index():
+        response = jsonify(message="The server is running")
+        return response
 
 #########################################################################################################################
-	
-	#@app.route("/signin",methods=["POST"])
-	@app.route("/signin")
-	def signinpage():
-		cursor = dbconn.cursor()
-		#uids= request.json['u_id']
-		#password= request.json['pwd']
-		#typess= request.json['type']
-		
-		uids= 'B190700CS'
-		password= 'wassup'
-		typess= 'student'
+    
+    #@app.route("/signin",methods=["POST"])
+    @app.route("/signin")
+    def signinpage():
+        cursor = dbconn.cursor()
+        #uids= request.json['u_id']
+        #password= request.json['pwd']
+        #typess= request.json['type']
+        
+        uids= 'B190SDtestCS'
+        password= 'SDtestPass'
+        typess= 'student'
 
-		#password encryption:
-		password=hashlib.sha256(password.encode('utf-8')).hexdigest() #hashvalue
+        #password encryption:
+        password=hashlib.sha256(password.encode('utf-8')).hexdigest() #hashvalue
 
-		cursor.execute("SELECT pwd from Users where u_id=%s",(uids,))
-		x=cursor.fetchone()
-		if x is None:
-			return jsonify(message="Complete the registration")
-		
-		temp= x[0]
-		dbconn.commit()
-		
+        cursor.execute("SELECT pwd from Users where u_id=%s",(uids,))
+        x=cursor.fetchone()
+        if x is None:
+            return jsonify(message="Complete the registration")
+        
+        temp= x[0]
+        dbconn.commit()
+        
 
-		if temp:
-			passcode=temp
-			if passcode==password:
-				if typess=='student':
-					cursor = dbconn.cursor()
-					cursor.execute("SELECT deptid from Student where roll_no=%s",(uids,))
-					deptids= cursor.fetchone()[0]
-					dbconn.commit()
-					
-					cursor = dbconn.cursor()
-					cursor.execute("SELECT dname from Departments where department_id=%s",(deptids,))
-					depts= cursor.fetchone()[0]
-					dbconn.commit()
-					
-					cursor = dbconn.cursor()
-					cursor.execute("SELECT * from Users where u_id=%s",(uids,))
-					dbconn.commit()
-					tempone= cursor.fetchone()
-					uids,names,emails,password,mobilenos=tempone
-					
-					return jsonify({"u_id":uids,"uname":names,"email":emails,"pwd":password,"mobileno":mobilenos,"deptid":deptids,"dname":depts})
+        if temp:
+            passcode=temp
+            if passcode==password:
+                if typess=='student':
+                    cursor = dbconn.cursor()
+                    cursor.execute("SELECT deptid from Student where roll_no=%s",(uids,))
+                    deptids= cursor.fetchone()[0]
+                    dbconn.commit()
+                    
+                    cursor = dbconn.cursor()
+                    cursor.execute("SELECT dname from Departments where department_id=%s",(deptids,))
+                    depts= cursor.fetchone()[0]
+                    dbconn.commit()
+                    
+                    cursor = dbconn.cursor()
+                    cursor.execute("SELECT * from Users where u_id=%s",(uids,))
+                    dbconn.commit()
+                    tempone= cursor.fetchone()
+                    uids,names,emails,password,mobilenos=tempone
+                    
+                    return jsonify({"u_id":uids,"uname":names,"email":emails,"pwd":password,"mobileno":mobilenos,"deptid":deptids,"dname":depts})
 
-				elif typess=='faculty':
-					cursor = dbconn.cursor()
-					cursor.execute("SELECT deptid from Faculty where ssn=%s",(uids,))
-					deptids= cursor.fetchone()[0]
-					dbconn.commit()
-					
-					cursor = dbconn.cursor()
-					cursor.execute("SELECT dname from Departments where department_id=%s",(deptids,))
-					depts= cursor.fetchone()[0]
-					dbconn.commit()
-					
-					cursor = dbconn.cursor()
-					cursor.execute("SELECT * from Users where u_id=%s",(uids,))
-					dbconn.commit()
-					tempone= cursor.fetchone()
-					uids,names,emails,password,mobilenos=tempone
-					
-					return jsonify({"u_id":uids,"uname":names,"email":emails,"pwd":password,"mobileno":mobilenos,"deptid":deptids,"dname":depts})
+                elif typess=='faculty':
+                    cursor = dbconn.cursor()
+                    cursor.execute("SELECT deptid from Faculty where ssn=%s",(uids,))
+                    deptids= cursor.fetchone()[0]
+                    dbconn.commit()
+                    
+                    cursor = dbconn.cursor()
+                    cursor.execute("SELECT dname from Departments where department_id=%s",(deptids,))
+                    depts= cursor.fetchone()[0]
+                    dbconn.commit()
+                    
+                    cursor = dbconn.cursor()
+                    cursor.execute("SELECT * from Users where u_id=%s",(uids,))
+                    dbconn.commit()
+                    tempone= cursor.fetchone()
+                    uids,names,emails,password,mobilenos=tempone
+                    
+                    return jsonify({"u_id":uids,"uname":names,"email":emails,"pwd":password,"mobileno":mobilenos,"deptid":deptids,"dname":depts})
 
 
-			else:
-					return jsonify(message="Incorrect Password")
-					
+            else:
+                    return jsonify(message="Incorrect Password")
+                    
 #########################################################################################################################
 
-	#@app.route("/signup",methods=["POST"])
-	@app.route("/signup")
-	def registration():
-	
-		cursor = dbconn.cursor()
-		#uids= request.json['u_id']
-		#names= request.json['uname'] #name 
-		#emails= request.json['email']
-		#password= request.json['pwd']
-		#mobilenos= request.json['mobileno']
-		#depts= request.json['dname']
-		#typess= request.json['type']
-		
-		uids= 'B190700CS'
-		names= 'Jesvin Jose'
-		emails= 'jeswin@gmail.com'
-		password= 'wassup'
-		mobilenos= '1234567777'
-		depts= 'EEE'
-		typess= 'student'
-		
-		#password encryption:
-		password=hashlib.sha256(password.encode('utf-8')).hexdigest() #hashvalue
-		
-		#insert data into user db
-		cursor.execute("INSERT INTO Users (u_id, uname, email, pwd, mobileno) VALUES(%s, %s, %s, %s, %s)",(uids, names, emails, password, mobilenos))
-		dbconn.commit()
-		
-		#getting dept id from department db
-		cursor = dbconn.cursor()
-		cursor.execute("SELECT department_id from Departments where dname=%s",(depts,))
-		deptids= cursor.fetchone()[0]
-		dbconn.commit()
-		
-		if typess=='student':
-			cursor = dbconn.cursor()
-			cursor.execute("INSERT INTO Student (roll_no, deptid) VALUES(%s, %s)",(uids, deptids))
-			dbconn.commit()
+    #@app.route("/signup",methods=["POST"])
+    @app.route("/signup")
+    def registration():
+    
+        cursor = dbconn.cursor()
+        #uids= request.json['u_id']
+        #names= request.json['uname'] #name 
+        #emails= request.json['email']
+        #password= request.json['pwd']
+        #mobilenos= request.json['mobileno']
+        #depts= request.json['dname']
+        #typess= request.json['type']
+        
+        uids= 'B190SDtestCS'
+        names= 'SDtestName'
+        emails= 'SDtestName@gmail.com'
+        password= 'SDtestPass'
+        mobilenos= '1239991111'
+        depts= 'CSE'
+        typess= 'student'
+        
+        #password encryption:
+        password=hashlib.sha256(password.encode('utf-8')).hexdigest() #hashvalue
+        
+        #insert data into user db
+        cursor.execute("INSERT INTO Users (u_id, uname, email, pwd, mobileno) VALUES(%s, %s, %s, %s, %s)",(uids, names, emails, password, mobilenos))
+        dbconn.commit()
+        
+        #getting dept id from department db
+        cursor = dbconn.cursor()
+        cursor.execute("SELECT department_id from Departments where dname=%s",(depts,))
+        deptids= cursor.fetchone()[0]
+        dbconn.commit()
+        
+        if typess=='student':
+            cursor = dbconn.cursor()
+            cursor.execute("INSERT INTO Student (roll_no, deptid) VALUES(%s, %s)",(uids, deptids))
+            dbconn.commit()
 
-			return jsonify({"u_id":uids,"uname":names,"email":emails,"pwd":password,"mobileno":mobilenos,"deptid":deptids,"dname":depts})
+            return jsonify({"u_id":uids,"uname":names,"email":emails,"pwd":password,"mobileno":mobilenos,"deptid":deptids,"dname":depts})
 
-		elif typess=='faculty':
-			cursor = dbconn.cursor()
-			cursor.execute("INSERT INTO Faculty (ssn, deptid) VALUES(%s, %s)",(uids, deptids))
-			dbconn.commit()
+        elif typess=='faculty':
+            cursor = dbconn.cursor()
+            cursor.execute("INSERT INTO Faculty (ssn, deptid) VALUES(%s, %s)",(uids, deptids))
+            dbconn.commit()
 
-			return jsonify({"u_id":uids,"uname":names,"email":emails,"pwd":password,"mobileno":mobilenos,"deptid":deptids,"dname":depts})
+            return jsonify({"u_id":uids,"uname":names,"email":emails,"pwd":password,"mobileno":mobilenos,"deptid":deptids,"dname":depts})
 
+#########################################################################################################################
 
+    #@app.route("/list_all_fac",methods=["POST"])
+    @app.route("/list_all_fac")
+    def listAllFacPage():
+        cursor = dbconn.cursor()
+        cursor.execute("SELECT ssn from Faculty")
+        list_of_ssn=cursor.fetchall()
+        dbconn.commit()
+
+        list_of_uname=[]
+        for i in list_of_ssn:
+            cursor = dbconn.cursor()
+            cursor.execute("SELECT uname FROM Users WHERE u_id=%s",(i[0],))
+            uname=cursor.fetchone()[0]
+            list_of_uname.append(uname)
+            dbconn.commit()
+
+        return jsonify(list_of_uname)
 
 ########################################  STUDENT  ###############################################
 
 
 
 
-	return app
+    return app
