4538e01d76465af28d5a3cdb9f3e842ff2b0514d
lenoahchacko@gmail.com <lenoahchacko@gmail.com>
2021-12-02 17:22:50 +0530

    optimized getfacs, added sample inputs for testing

--- __init__.py
+++ __init__.py
@@ -47,10 +47,15 @@ def index():
     return response
 
 #########################################################################################################################
-
+#Signs all kinds of users in
 @app.route("/signin",methods=["POST"])
 def signinpage():
     cursor = dbconn.cursor()
+    """ {
+    "u_id":"B190SDtestCS",
+    "pwd":"SDtestPass",
+    "type":"student"
+    } """
     uids= request.json['u_id']
     password= request.json['pwd']
     typess= request.json['type']
@@ -163,28 +168,31 @@ def registration():
         return jsonify({"u_id":uids,"uname":names,"email":emails,"pwd":password,"mobileno":mobilenos,"deptid":deptids,"dname":depts})
 
 #########################################################################################################################
-
+#Gets the id and username of all the faculties
 @app.route("/list_all_fac",methods=["POST"])
 def listAllFacPage():
     cursor = dbconn.cursor()
     list_of_uname=[]
     cursor = dbconn.cursor()
-    cursor.execute("SELECT uname FROM Users u, Faculty f WHERE u.u_id=f.ssn")
+    cursor.execute("SELECT u.u_id,u.uname FROM Users u, Faculty f WHERE u.u_id=f.ssn")
     list_of_uname=cursor.fetchall()
-    print(list_of_uname)
+    response=list(map(lambda x: {"u_id":x[0],"uname":x[1]},list_of_uname))
+    print(response)
     dbconn.commit()
 
-    return jsonify(list_of_uname)
+    return jsonify(response)
 
 #########################################################################################################################
-
-#@app.route("/details",methods=["POST"])
-@app.route("/details")
+#Gets details of a specific user
+@app.route("/details",methods=["POST"])
 def details():
+    """ {
+    "u_id":"123"
+    } """
     cursor = dbconn.cursor()
-    u_id = "B190SDtestCS"
+    uid= request.json['u_id']
     #u_id= request.json['u_id']
-    cursor.execute("SELECT * from Users where u_id=%s",(u_id,))
+    cursor.execute("SELECT * from Users where u_id=%s",(uid,))
     details=cursor.fetchone()
     dbconn.commit()
     u_id,names,emails,password,mobilenos=details
@@ -194,25 +202,25 @@ def details():
 ########################################  STUDENT  ###############################################
 
 #insert into the appointment table
-#@app.route("/request_stud",methods=["POST"])
-@app.route("/request_stud")
+@app.route("/request_stud",methods=["POST"])
 def request_stud():
-    #Details that we take in: date_created, date_appointment, time_appointment, title, description, stud_id, fac_id
     cursor = dbconn.cursor()
-    date_created = "2020-12-30"
-    date_appointment = "2020-12-31"
-    time_appointment = "10:00:00"
-    title = "test3"
-    description = "3rd test description"
-    stud_id = "B190SDtestCS"
-    fac_id = "123"
-    #date_created= request.json['date_created']
-    #date_appointment= request.json['date_appointment']
-    #time_appointment= request.json['time_appointment']
-    #title= request.json['title']
-    #description= request.json['description']
-    #stud_id= request.json['stud_id']
-    #fac_id= request.json['fac_id']
+    """ {
+    "date_created" : "2020-12-30",
+    "date_appointment" : "2020-12-31",
+    "time_appointment" : "10:00:00",
+    "title" : "test3",
+    "description" : "3rd test description",
+    "stud_id" : "B190SDtestCS",
+    "fac_id" : "123"
+    } """
+    date_created= request.json['date_created']
+    date_appointment= request.json['date_appointment']
+    time_appointment= request.json['time_appointment']
+    title= request.json['title']
+    description= request.json['description']
+    stud_id= request.json['stud_id']
+    fac_id= request.json['fac_id']
     dateTime = date_appointment + "#" + time_appointment
     # we are concatinating the date and time to get the datetime
     # because our database only has one column for datetime
